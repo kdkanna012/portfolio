fcdaf6010dce07821cdd1961098a3a49831dd979
Kanna Dasan <kannadasan1931@gmail.com>
2021-11-27 22:38:02 +0530

    first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'kannadasan1931@gmail.com'
+EMAIL_HOST_PASSWORD = 'ubtjhvtkddbninsn'
 
 django_heroku.settings(locals())
